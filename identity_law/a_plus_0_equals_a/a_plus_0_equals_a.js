9890ddba3685f0e6fc452d475768f52e34c094ae
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-01-09 17:05:13 -0500

    update

---
+++ identity_law/a_plus_0_equals_a/a_plus_0_equals_a.js
@@ -0,0 +1,21 @@
+// a_plus_0_equals_a.js
+// A + 0 = A
+
+// define a variable A
+let A = true;
+
+// applying A + 0 = A
+// A OR 0 (false) gives us A
+let result = A || false;
+
+// result will be the same as A
+console.log(result);
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
